aae92b62cbf17ecbc99d1d17af91b5eea7715b30
Joseph Hajjar <jhhajjar@gmail.com>
2025-10-24 23:50:14 -0700

    add cors

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/articles")
 def articles():
     df = pd.read_csv('data/articles.csv')
     df = df.sort_values(by='date_posted')
     return df.to_json(orient='records')
-
