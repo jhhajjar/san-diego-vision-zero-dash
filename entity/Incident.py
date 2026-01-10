bc3075f880bcd0e8301533a7ec3750a85c79f348
Joseph Hajjar <jhhajjar@gmail.com>
2026-01-10 11:21:25 -0800

    add incident entity

---
+++ entity/Incident.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from enum import Enum
+
+
+class HitRunLevel(Enum):
+    MISDEMEANOR = "Misdemeanor"
+    FELONY = "Felony"
+    NONE = "None"
+
+
+class IncidentDTO:
+    report_id: str
+    date_time: datetime
+    police_beat: int
+    address_no_primary: int
+    address_pd_primary: str
+    address_road_primary: str
+    address_sfx_primary: str
+    address_pd_intersecting: str
+    address_name_intersecting: str
+    address_sfx_intersecting: str
+    violation_section: str
+    violation_type: str
+    charge_desc: str
+    injured: int
+    killed: int
+    hit_run_lvl: HitRunLevel
+    neighborhood: str
+    full_address: str
+
+
+class IncidentListDTO:
+    incidents: list[IncidentDTO]
+    page: int
+    page_size: int
+    total_incidents: int
+
+
+class IncidentMetadataDTO:
+    number_of_days_since_last_incident: int
+    latest_date: datetime
