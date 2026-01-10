b2917392743d72894f598b459e6f1b065560c026
Joseph Hajjar <jhhajjar@gmail.com>
2026-01-10 11:17:11 -0800

    working version

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 from services.aws_service import read_file_s3
+from services.pd_service import (
+    get_incident_metadata,
+    get_incidents,
+    map_incident_df_to_incident_list_dto,
+    read_pd_csv,
+    filter_for_casualties,
+)
 from flask_cors import CORS
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 CORS(app)
+load_dotenv()
 
 
 @app.route("/articles")
 def articles():
-    load_dotenv()
     # fetch df
     df = read_file_s3(os.getenv("S3_ARTICLES_FILENAME"))
     # filter out non relevant articles
@@ -18,3 +25,28 @@ def articles():
     # sort by date
     df = df.sort_values(by="date_posted", ascending=False)
     return df.to_json(orient="records")
+
+
+@app.route("/incidents")
+def incidents():
+    # Parse pagination parameters
+    page = request.args.get("page", default=1, type=int)
+    page_size = request.args.get("page_size", default=10, type=int)
+
+    # Fetch paginated incidents
+    incidents_df = get_incidents(page=page, page_size=page_size)
+
+    # Map to DTO
+    mapped_response = map_incident_df_to_incident_list_dto(
+        incidents_df, page, page_size
+    )
+
+    return jsonify(vars(mapped_response)), 200
+
+
+@app.route("/incidents/metadata")
+def incident_metadata():
+    df = read_pd_csv()
+    filtered_df = filter_for_casualties(df)
+    metadata = get_incident_metadata(filtered_df)
+    return jsonify(vars(metadata)), 200
--- services/pd_service.py
+++ services/pd_service.py
@@ -1,4 +1,7 @@
+import re
 import pandas as pd
+
+from entity.Incident import IncidentDTO, IncidentListDTO, IncidentMetadataDTO
 
 
 def read_pd_csv() -> pd.DataFrame:
@@ -34,6 +37,8 @@ def read_pd_csv() -> pd.DataFrame:
     mapping = beats_df.set_index("beat")["neighborhood"]
     # Create the new column in df1 by mapping the 'beat' column
     incident_df["neighborhood"] = incident_df["police_beat"].map(mapping)
+    incident_df["hit_run_lvl"] = incident_df["hit_run_lvl"].fillna("NONE")
+    incident_df["date_time"] = pd.to_datetime(incident_df["date_time"])
     return incident_df.sort_values(by="date_time", ascending=False)
 
 
@@ -53,8 +58,90 @@ def paginate(df: pd.DataFrame, page: int = 1, page_size: int = 10) -> pd.DataFra
     return df[start:end]
 
 
+def map_date_time_to_string(date_time: pd.Timestamp) -> str:
+    return date_time.strftime("%Y-%m-%d")
+
+
+def map_incident_dict_to_incident_dto(incident_dicts: list[dict]) -> list[IncidentDTO]:
+    incident_dtos = []
+    for incident_dict in incident_dicts:
+        incident_dto = IncidentDTO()
+        incident_dto.report_id = incident_dict.get("report_id")
+        incident_dto.date_time = map_date_time_to_string(incident_dict.get("date_time"))
+        incident_dto.police_beat = incident_dict.get("police_beat")
+        incident_dto.address_no_primary = incident_dict.get("address_no_primary")
+        incident_dto.address_pd_primary = incident_dict.get("address_pd_primary")
+        incident_dto.address_road_primary = incident_dict.get("address_road_primary")
+        incident_dto.address_sfx_primary = incident_dict.get("address_sfx_primary")
+        incident_dto.address_pd_intersecting = incident_dict.get(
+            "address_pd_intersecting"
+        )
+        incident_dto.address_name_intersecting = incident_dict.get(
+            "address_name_intersecting"
+        )
+        incident_dto.address_sfx_intersecting = incident_dict.get(
+            "address_sfx_intersecting"
+        )
+        incident_dto.violation_section = incident_dict.get("violation_section")
+        incident_dto.violation_type = incident_dict.get("violation_type")
+        incident_dto.charge_desc = incident_dict.get("charge_desc")
+        incident_dto.injured = incident_dict.get("injured")
+        incident_dto.killed = incident_dict.get("killed")
+        incident_dto.hit_run_lvl = incident_dict.get("hit_run_lvl")
+        incident_dto.neighborhood = incident_dict.get("neighborhood")
+        incident_dto.full_address = incident_dict.get("full_address")
+        incident_dtos.append(vars(incident_dto))
+    return incident_dtos
+
+
 def get_incidents(page: int = 1, page_size: int = 10) -> pd.DataFrame:
     df = read_pd_csv()
     filtered_df = filter_for_casualties(df)
-    paginated_df = paginate(filtered_df, page, page_size)
-    return paginated_df.to_json(orient="records")
+    df_with_addresses = parse_full_address(filtered_df)
+    paginated_df = paginate(df_with_addresses, page, page_size)
+    return paginated_df
+
+
+def get_incident_metadata(df: pd.DataFrame) -> IncidentMetadataDTO:
+    dto_object = IncidentMetadataDTO()
+    dto_object.latest_date = df["date_time"].max()
+    dto_object.number_of_days_since_last_incident = (
+        pd.Timestamp.now() - df["date_time"].max()
+    ).days
+    return dto_object
+
+
+def map_incident_df_to_incident_list_dto(
+    df: pd.DataFrame, page: int, page_size: int
+) -> IncidentListDTO:
+    dto_object = IncidentListDTO()
+    dto_object.incidents = map_incident_dict_to_incident_dto(
+        df.to_dict(orient="records")
+    )
+    dto_object.page = page
+    dto_object.page_size = page_size
+    dto_object.total_incidents = len(df)
+    return dto_object
+
+
+def parse_full_address(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Parses the full address from the dataframe.
+    """
+
+    def construct_full_address(row):
+        address = f"{row['address_pd_primary']} {row['address_road_primary']} {row['address_sfx_primary']}"
+        if row["address_no_primary"] > 0:
+            # If there is a st number, there is no intersection
+            address = f"{row['address_no_primary']}" + " " + address
+        else:
+            # else use the intersecting st
+            address += (
+                " and "
+                + f"{row['address_pd_intersecting']} {row['address_name_intersecting']} {row['address_sfx_intersecting']}"
+            )
+        address = address + ", SAN DIEGO, CA"
+        return re.sub(" +", " ", address)
+
+    df["full_address"] = df.apply(construct_full_address, axis=1)
+    return df
